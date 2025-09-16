cfecc6899f77647a03aa8e0f04688b10aa423708
Animesh478 <animeshmohanty.am@gmail.com>
2025-09-16 08:20:05 +0530

    serving static files with express.static

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const PORT = require("./env");
+const path = require("node:path");
 
 const app = express();
+
+// app.use(express.static(`${__dirname}/public`));
+
+const staticPath = path.join(__dirname, "public");
+app.use(express.static(staticPath));
+// Note: always use absolute path static() method but it can be done with relative path also
+
+// Both the ways above use absolute path, but the second one is platform independent
 
 // const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
